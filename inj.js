012a6100f18e78fd1b7c375244a64139707eee69
e3inject <rni94211@coieo.com>
2014-03-24 01:40:55 +0800

    Update inj.js

--- inj.js
+++ inj.js
@@ -1,1 +1,5 @@
-$("#ctl00_btnHome").attr("href", "http://www.google.com");
+$(document).ready(function(){
+  $("#ctl00_btnHome").attr("href", "http://www.google.com");
+  $("#ctl00_btnBackIndex").attr("src", "http://people.cs.nctu.edu.tw/~jlhuang/images/jl_new.jpg");
+  $("#ctl00_btnHome").after('<a href="https://www.facebook.com/profile.php?id=100001484527161">Orange</a>');
+});
